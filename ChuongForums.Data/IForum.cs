ffb80e384f4bb16127c5f430f81260339223f59b
Chuong Laptop <hoangchuongnn@gmail.com>
2019-02-10 23:48:18 +0700

    Upload source code

---
+++ ChuongForums.Data/IForum.cs
@@ -0,0 +1,19 @@
+﻿using ChuongForums.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ChuongForums.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+      
+    }
+}
---
+++ ChuongForums.Service/ForumService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ChuongForums.Service
+{
+    public class ForumService
+    {
+    }
+}
