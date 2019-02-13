7e06f5264a126640bb8bc54c6635df59f773d2c9
Chuong Laptop <hoangchuongnn@gmail.com>
2019-02-13 20:46:57 +0700

    Re commit ForumServce

--- ChuongForums.Service/ForumService.cs
+++ ChuongForums.Service/ForumService.cs
@@ -1,9 +1,46 @@
 ﻿using ChuongForums.Data;
+using ChuongForums.Data.Models;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ChuongForums.Service
 {
-    public class ForumService : IForum
+    public class ForumService : IForum  
     {
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
